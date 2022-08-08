d21402dffbeef083a2fc8bc64f7c9fc47964d79e
Charlzk <104715127+Charlzk05@users.noreply.github.com>
2022-08-08 17:24:57 +0800

    Add files via upload

--- Replit Browser App/MainForm.Designer.cs
+++ Replit Browser App/MainForm.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.backToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.forwardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sfdToolStripMenuItem = new System.Windows.Forms.ToolStripSeparator();
@@ -38,7 +39,11 @@
             this.loginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.topMostToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mainBrowser = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.URL = new System.Windows.Forms.Label();
+            this.statusCode = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -48,6 +53,7 @@
             this.loginToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.menuStrip1.Size = new System.Drawing.Size(1064, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
@@ -55,6 +61,7 @@
             // fToolStripMenuItem
             // 
             this.fToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refreshToolStripMenuItem,
             this.backToolStripMenuItem,
             this.forwardToolStripMenuItem,
             this.sfdToolStripMenuItem,
@@ -62,6 +69,13 @@
             this.fToolStripMenuItem.Name = "fToolStripMenuItem";
             this.fToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
             this.fToolStripMenuItem.Text = "Browser";
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
             // 
             // backToolStripMenuItem
             // 
@@ -113,10 +127,43 @@
             this.mainBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mainBrowser.Location = new System.Drawing.Point(0, 24);
             this.mainBrowser.Name = "mainBrowser";
-            this.mainBrowser.Size = new System.Drawing.Size(1064, 657);
+            this.mainBrowser.Size = new System.Drawing.Size(1064, 632);
             this.mainBrowser.TabIndex = 1;
+            this.mainBrowser.TitleChanged += new System.EventHandler<CefSharp.TitleChangedEventArgs>(this.mainBrowser_TitleChanged);
             this.mainBrowser.FrameLoadStart += new System.EventHandler<CefSharp.FrameLoadStartEventArgs>(this.mainBrowser_FrameLoadStart);
             this.mainBrowser.FrameLoadEnd += new System.EventHandler<CefSharp.FrameLoadEndEventArgs>(this.mainBrowser_FrameLoadEnd);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.URL);
+            this.panel1.Controls.Add(this.statusCode);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 656);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1064, 25);
+            this.panel1.TabIndex = 2;
+            // 
+            // URL
+            // 
+            this.URL.AutoSize = true;
+            this.URL.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.URL.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.URL.Location = new System.Drawing.Point(511, 5);
+            this.URL.Name = "URL";
+            this.URL.Size = new System.Drawing.Size(31, 15);
+            this.URL.TabIndex = 1;
+            this.URL.Text = "URL:";
+            // 
+            // statusCode
+            // 
+            this.statusCode.AutoSize = true;
+            this.statusCode.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusCode.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.statusCode.Location = new System.Drawing.Point(12, 5);
+            this.statusCode.Name = "statusCode";
+            this.statusCode.Size = new System.Drawing.Size(42, 15);
+            this.statusCode.TabIndex = 0;
+            this.statusCode.Text = "Status:";
             // 
             // MainForm
             // 
@@ -124,16 +171,22 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1064, 681);
             this.Controls.Add(this.mainBrowser);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HelpButton = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Replit Browser App";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,12 +197,16 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem backToolStripMenuItem;
-        private CefSharp.WinForms.ChromiumWebBrowser mainBrowser;
         private System.Windows.Forms.ToolStripMenuItem forwardToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem loginToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem topMostToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem inspectElementToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator sfdToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private CefSharp.WinForms.ChromiumWebBrowser mainBrowser;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label statusCode;
+        private System.Windows.Forms.Label URL;
     }
 }
 
--- Replit Browser App/MainForm.cs
+++ Replit Browser App/MainForm.cs
@@ -1,7 +1,6 @@
 ﻿using CefSharp;
 using CefSharp.WinForms;
 using System;
-using System.ComponentModel;
 using System.IO;
 using System.Windows.Forms;
 
@@ -18,14 +17,21 @@ namespace Replit_Browser_App
         private void InitializedBrowser()
         {
             CefSettings settings = new CefSettings();
+
             settings.CachePath = Path.GetFullPath("./Cache");
             Cef.Initialize(settings);
+
             mainBrowser.LoadUrl("https://replit.com/login");
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mainBrowser.Refresh();
         }
 
         private void backToolStripMenuItem_Click(object sender, EventArgs e)
@@ -36,6 +42,11 @@ namespace Replit_Browser_App
         private void forwardToolStripMenuItem_Click(object sender, EventArgs e)
         {
             mainBrowser.Forward();
+        }
+
+        private void inspectElementToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mainBrowser.ShowDevTools();
         }
 
         private void topMostToolStripMenuItem_Click(object sender, EventArgs e)
@@ -50,19 +61,30 @@ namespace Replit_Browser_App
             }
         }
 
-        private void inspectElementToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            mainBrowser.ShowDevTools();
-        }
+        // Events!
 
         private void mainBrowser_FrameLoadStart(object sender, FrameLoadStartEventArgs e)
         {
-            mainBrowser.Invoke((MethodInvoker)(() => this.Text = "Replit Browser App | Loading..."));
+            mainBrowser.Invoke((MethodInvoker)(() => statusCode.Text = $"Status: Loading..."));
         }
 
         private void mainBrowser_FrameLoadEnd(object sender, FrameLoadEndEventArgs e)
         {
-            mainBrowser.Invoke((MethodInvoker)(() => this.Text = "Replit Browser App"));
+            mainBrowser.Invoke((MethodInvoker)(() => statusCode.Text = $"Status: {e.HttpStatusCode}"));
+            mainBrowser.Invoke((MethodInvoker)(() => URL.Text = $"URL: {e.Url}"));
+        }
+
+        private void mainBrowser_TitleChanged(object sender, TitleChangedEventArgs e)
+        {
+            mainBrowser.Invoke((MethodInvoker)(() => this.Text = $"Replit Browser App | {e.Title}"));
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode.ToString() == "D")
+            {
+                MessageBox.Show("True");
+            }
         }
     }
 }
