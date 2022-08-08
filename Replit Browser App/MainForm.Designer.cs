df8c2d6feb3d2e9daa18915ce1d703926db34002
Charlzk <104715127+Charlzk05@users.noreply.github.com>
2022-08-08 12:05:13 +0800

    Add files via upload

---
+++ Replit Browser App/MainForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Replit_Browser_App
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.forwardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sfdToolStripMenuItem = new System.Windows.Forms.ToolStripSeparator();
+            this.inspectElementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.topMostToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainBrowser = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fToolStripMenuItem,
+            this.loginToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1064, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fToolStripMenuItem
+            // 
+            this.fToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backToolStripMenuItem,
+            this.forwardToolStripMenuItem,
+            this.sfdToolStripMenuItem,
+            this.inspectElementToolStripMenuItem});
+            this.fToolStripMenuItem.Name = "fToolStripMenuItem";
+            this.fToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.fToolStripMenuItem.Text = "Browser";
+            // 
+            // backToolStripMenuItem
+            // 
+            this.backToolStripMenuItem.Name = "backToolStripMenuItem";
+            this.backToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.backToolStripMenuItem.Text = "Back";
+            this.backToolStripMenuItem.Click += new System.EventHandler(this.backToolStripMenuItem_Click);
+            // 
+            // forwardToolStripMenuItem
+            // 
+            this.forwardToolStripMenuItem.Name = "forwardToolStripMenuItem";
+            this.forwardToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.forwardToolStripMenuItem.Text = "Forward";
+            this.forwardToolStripMenuItem.Click += new System.EventHandler(this.forwardToolStripMenuItem_Click);
+            // 
+            // sfdToolStripMenuItem
+            // 
+            this.sfdToolStripMenuItem.Name = "sfdToolStripMenuItem";
+            this.sfdToolStripMenuItem.Size = new System.Drawing.Size(155, 6);
+            // 
+            // inspectElementToolStripMenuItem
+            // 
+            this.inspectElementToolStripMenuItem.Name = "inspectElementToolStripMenuItem";
+            this.inspectElementToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.inspectElementToolStripMenuItem.Text = "Inspect Element";
+            this.inspectElementToolStripMenuItem.Click += new System.EventHandler(this.inspectElementToolStripMenuItem_Click);
+            // 
+            // loginToolStripMenuItem
+            // 
+            this.loginToolStripMenuItem.Checked = true;
+            this.loginToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.loginToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.topMostToolStripMenuItem});
+            this.loginToolStripMenuItem.Name = "loginToolStripMenuItem";
+            this.loginToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.loginToolStripMenuItem.Text = "Settings";
+            // 
+            // topMostToolStripMenuItem
+            // 
+            this.topMostToolStripMenuItem.CheckOnClick = true;
+            this.topMostToolStripMenuItem.Name = "topMostToolStripMenuItem";
+            this.topMostToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.topMostToolStripMenuItem.Text = "Top Most";
+            this.topMostToolStripMenuItem.Click += new System.EventHandler(this.topMostToolStripMenuItem_Click);
+            // 
+            // mainBrowser
+            // 
+            this.mainBrowser.ActivateBrowserOnCreation = false;
+            this.mainBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainBrowser.Location = new System.Drawing.Point(0, 24);
+            this.mainBrowser.Name = "mainBrowser";
+            this.mainBrowser.Size = new System.Drawing.Size(1064, 657);
+            this.mainBrowser.TabIndex = 1;
+            this.mainBrowser.FrameLoadStart += new System.EventHandler<CefSharp.FrameLoadStartEventArgs>(this.mainBrowser_FrameLoadStart);
+            this.mainBrowser.FrameLoadEnd += new System.EventHandler<CefSharp.FrameLoadEndEventArgs>(this.mainBrowser_FrameLoadEnd);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 681);
+            this.Controls.Add(this.mainBrowser);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Replit Browser App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backToolStripMenuItem;
+        private CefSharp.WinForms.ChromiumWebBrowser mainBrowser;
+        private System.Windows.Forms.ToolStripMenuItem forwardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loginToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem topMostToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inspectElementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator sfdToolStripMenuItem;
+    }
+}
+
---
+++ Replit Browser App/MainForm.cs
@@ -0,0 +1,68 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Replit_Browser_App
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializedBrowser();
+        }
+
+        private void InitializedBrowser()
+        {
+            CefSettings settings = new CefSettings();
+            settings.CachePath = Path.GetFullPath("./Cache");
+            Cef.Initialize(settings);
+            mainBrowser.LoadUrl("https://replit.com/login");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backToolStripMenuItem_Click(object sender, EventArgs e)
+        {   
+            mainBrowser.Back();
+        }
+
+        private void forwardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mainBrowser.Forward();
+        }
+
+        private void topMostToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (topMostToolStripMenuItem.Checked == true)
+            {
+                this.TopMost = true;
+            }
+            else
+            {
+                this.TopMost = false;
+            }
+        }
+
+        private void inspectElementToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mainBrowser.ShowDevTools();
+        }
+
+        private void mainBrowser_FrameLoadStart(object sender, FrameLoadStartEventArgs e)
+        {
+            mainBrowser.Invoke((MethodInvoker)(() => this.Text = "Replit Browser App | Loading..."));
+        }
+
+        private void mainBrowser_FrameLoadEnd(object sender, FrameLoadEndEventArgs e)
+        {
+            mainBrowser.Invoke((MethodInvoker)(() => this.Text = "Replit Browser App"));
+        }
+    }
+}
